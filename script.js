54622e4b421d412e4c17970ed503cd3e1eaeb37a
Ilya <example@gmail.com>
2021-02-16 21:16:45 +0600

    Cycles added

--- script.js
+++ script.js
@@ -2,17 +2,29 @@
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотерли?');
 
 let personalMovieDB = {
-count:numberOfFilms,
-movies:{},
-actors:{},
-genres:[],
-privat:false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-//alert(numberOfFilms);
-//alert(personalMovieDB.count);
+if (personalMovieDB.count<10) {alert("Просмотрено довольно мало фильмов");}
+    else if (personalMovieDB.count>=10 && personalMovieDB.count<=30) {alert("Вы классический зритель");}
+    else if(personalMovieDB.count>30) {alert("Вы киноман");}
+    else {alert("Произошла ошибка");}
 
-let movieName = prompt('Один из последних просмотренных фильмов?');
-personalMovieDB.movies.movieName = prompt('На сколько оцените его?');
+let i = 0;
 
-alert(personalMovieDB.movies.movieName);
+while (i<2) {
+    let movieName = prompt('Один из последних просмотренных фильмов?');
+    if(movieName=="" || movieName==null || movieName.length>50) continue;
+
+    let a = personalMovieDB.movies[movieName] = prompt('На сколько оцените его?'); 
+    if(a=="" || a==null || a.length>50) continue;
+    
+    i++;
+}
+
+console.log(personalMovieDB);
+
